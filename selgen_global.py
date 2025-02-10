56adb22e89a29473b74b5e9558464a72632fa53b
Tadeáš Fryčák <tadeas@frycak.com>
2025-02-10 19:23:34 +0100

    feat: final onsite changes

--- selgen_global.py
+++ selgen_global.py
@@ -1,5 +1,5 @@
 import numpy as np
-path = '/Users/michal/Desktop/Dev/data/selgen_error/'
+path = 'images/'
 etalon_path = './etalon.mat'
 lower_thresh = np.array([25,60,50])
 upper_thresh = np.array([95,250,130])
---
+++ start.bat
@@ -0,0 +1,1 @@
+python selgen_analysis.py
